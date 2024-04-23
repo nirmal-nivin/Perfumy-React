1e8acf29045c1c75baaa4a75fffa35f1d076b7c2
nirmal-nivin <nirmalbkumar95@gmail.com>
2024-04-23 10:38:35 +0530

    updated project

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+//SEARCH BAR COMPONENTS
+function Search()
+{
+  return(
+    <div className="searchbar">
+      <input type="text" placeholder="search"/>
+    </div>
+  )
+}
+
+export default Search
